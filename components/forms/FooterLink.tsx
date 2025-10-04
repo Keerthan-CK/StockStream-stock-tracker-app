de8fd0c1f1cf0f02ecf7d2a241d301da5a9fa655
Keerthan-CK <keerthanck705703@gmail.com>
2025-10-04 08:33:33 +0530

    Production Ready

--- components/forms/FooterLink.tsx
+++ components/forms/FooterLink.tsx
@@ -1,17 +1,23 @@
-import { Link } from 'lucide-react'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+
+interface FooterLinkProps {
+  text: string;
+  linkText: string;
+  href: string;
+}
 
 const FooterLink = ({ text, linkText, href }: FooterLinkProps) => {
   return (
-    <div className='text-center pt-4'>
-      <p className='text-sm text-gray-500'>
-        {text}{` `}
-        <Link href={href} className="footer-link">
+    <div className="text-center pt-4">
+      <p className="text-sm text-gray-500">
+        {text}{" "}
+        <Link href={href} className="text-blue-500 hover:underline">
           {linkText}
         </Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default FooterLink
+export default FooterLink;
